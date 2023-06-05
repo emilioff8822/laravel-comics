536e0646eaa6f1f6ecc6eaf7aca7c7ea7b32791e
emilioff8822 <emilioff@hotmail.it>
2023-06-06 00:08:32 +0200

    commit-title-cards

---
+++ laravelcomics/resources/views/collectibles.blade.php
@@ -0,0 +1,14 @@
+@extends('main')
+@section('content')
+    <div class="container">
+
+        <h1>COLLECTIBLES</h1>
+        <div class="jumbotron">
+
+            <img class="logo" src="/img/jumbotron.jpg" alt="logo">
+
+        </div>
+    @endsection
+    @section('title')
+        Collectibles
+    @endsection
--- laravelcomics/resources/views/home.blade.php
+++ laravelcomics/resources/views/home.blade.php
@@ -1,17 +1,29 @@
 @extends('main')
 
 @section('content')
-    <div class="container">
-        <div class="jumbotron">
-            <img class="logo" src="/img/jumbotron.jpg" alt="logo">
-        </div>
-        <div class="card-container">
-            <div class="current-series">
-                CURRENT SERIES
+    <main class="bg-light">
+        <div class="container">
+            <div class="jumbotron">
+                <img class="logo" src="/img/jumbotron.jpg" alt="logo">
             </div>
-            <Card v-for="card in cards" :key="card.thumb" :thumb="card.thumb" :price="card.price"
-                :series="card.series" :type="card.type" />
-            <button class="load-more">LOAD MORE</button>
+            <div class="card-container">
+                <div class="current-series">
+                    CURRENT SERIES
+                </div>
+
+                @foreach ($datax['cardsData'] as $card)
+                    <div class="card">
+                        <div class="card-img-container">
+                            <img src="{{ $card['thumb'] }}" alt="{{ $card['series'] }}">
+                        </div>
+                        <p>{{ $card['price'] }}</p>
+                        <p>{{ $card['series'] }}</p>
+                        <p>{{ $card['type'] }}</p>
+                    </div>
+                @endforeach
+
+                <button class="load-more">LOAD MORE</button>
+            </div>
         </div>
-    </div>
+    </main>
 @endsection
